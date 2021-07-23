2d602e2c5dbc194e54f72c5dd40129355ac1f6f1
juma <juma.durduyev@gmail.com>
2021-07-22 21:46:52 -0700

    Initial Commit

---
+++ shuffle_menu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shuffle_menu
+//
+//  Created by Juma on 7/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
